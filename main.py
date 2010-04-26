6e99d2b21f2f85f55d131b7a0d706661dcfa8bcc
Daniel Vydra <usefulaccount@gmail.com>
2010-04-27 00:20:56 +0100

    initial version

---
+++ main.py
@@ -0,0 +1,67 @@
+from google.appengine.api import urlfetch
+from google.appengine.api.labs import taskqueue
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
+import BeautifulSoup
+import logging
+import os
+import urllib
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        soup = BeautifulSoup.BeautifulSoup(urlfetch.fetch(url='http://www.breakfastpolitics.com').content)
+        links = soup.findAll('div', {'class':'entry-content'})[0].findAll('a')
+        articles = []
+        for link in links:
+            linktext = link.string
+            url = link['href']
+            byline = unicode(link.nextSibling).strip()
+            articles.append({
+                'url': url,
+                'byline': byline,
+                'linktext': linktext,
+                }
+            )
+        template_values = {'articles': articles}
+        
+        path = os.path.join(os.path.dirname(__file__), 'list.html')
+        self.response.out.write(template.render(path, template_values))
+        
+    def post(self):
+        articles = self.request.get_all("articles")
+        for url in articles:
+           taskqueue.add(
+               url='/load-worker-dfsgylsdfgkjdfhlgjkdfdfgjfdslg', 
+               params={'url': url}
+           )
+        self.response.out.write("Sent %d articles to instapaper" % len(articles))
+        
+class LoadWorkerHandler(webapp.RequestHandler):
+    def post(self):
+        article_url = self.request.get('url')
+        form_fields = {
+          "username": self.request.get('username'),
+          "password": self.request.get('password'),
+          "url": article_url,
+          "auto-title": "1"
+        }
+        form_data = urllib.urlencode(form_fields)
+
+        instapaper_response = urlfetch.fetch(
+            url= "https://www.instapaper.com/api/add",
+            method= urlfetch.POST,
+            payload= form_data
+        )
+        logging.info("Lodged %s with instapaper. Reponse = %d" % (article_url, instapaper_response.status_code,))
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/', MainHandler),
+        ('/load-worker-dfsgylsdfgkjdfhlgjkdfdfgjfdslg', LoadWorkerHandler),        
+        ],
+        debug=True)
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
